03f4b379969f64b58426e3962b11750cbfe40a02
tanyavszabo <tatianavszabo@gmail.com>
2023-10-11 23:01:56 -0400

    Capstone-Spa updated

--- Components/Nav.js
+++ Components/Nav.js
@@ -2,19 +2,19 @@ import html from "html-literal";
 
 export default links => html`
   <div class="header">
-  <h1>COBALT</h1>
-</div>
-
-<div class="row">
-  <div class="col-3 col-s-3 menu">
-    <ul>
-      <li>Home </li>
-      <li>Connect</li>
-      <li>Collab</li>
-      <li>Create</li>
-      <li>About</li>
-      <li>Profile</li>
-
-    </ul>
+    <h1>COBALT</h1>
   </div>
 
+  <div class="row">
+    <div class="col-3 col-s-3 menu">
+      <ul>
+        <li>Home</li>
+        <li>Connect</li>
+        <li>Collab</li>
+        <li>Create</li>
+        <li>About</li>
+        <li>Profile</li>
+      </ul>
+    </div>
+  </div>
+`;
--- index.js
+++ index.js
@@ -4,17 +4,6 @@ import Navigo from "navigo";
 import { capitalize } from "lodash";
 import axios from "axios";
 
-function render() {
-  document.querySelector("#root").innerHTML = `
-      ${Header()}
-      ${Nav()}
-      ${Main()}
-      ${Footer()}
-    `;
-}
-
-render();
-
 const router = new Navigo("/");
 function render(state = store.Home) {
   document.querySelector("#root").innerHTML = `
@@ -75,8 +64,38 @@ router.hooks({
           .catch(err => {
             console.log(err);
             done();
+          });
+        break;
+      // Add a case for each view that needs data from an API
+      case "Pizza":
+        // New Axios get request utilizing already made environment variable
+        axios
+          .get(`${process.env.PIZZA_PLACE_API_URL}/pizza`)
+          .then(response => {
+            // We need to store the response to the state, in the next step but in the meantime let's see what it looks like so that we know what to store from the response.
+            store.Pizza.pizzas = response.data;
+            console.log(store.Pizza);
+            done();
+          })
 
+          .catch(error => {
+            console.log("It puked", error);
+            done();
+          });
+        break;
+      default:
+        done();
+    }
+  },
+  already: params => {
+    const view =
+      params && params.data && params.data.view
+        ? capitalize(params.data.view)
+        : "Home";
 
+    render(store[view]);
+  }
+});
 
 router
   .on({
@@ -92,3 +111,4 @@ router
     }
   })
   .resolve();
+
