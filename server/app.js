5e612339259f84c32fb6653e2effc32d2266c125
tanyavszabo <tatianavszabo@gmail.com>
2023-10-17 00:19:49 -0400

    Nelify update

--- server/app.js
+++ server/app.js
@@ -1,21 +1,25 @@
-/* app.js */
-// 'Import' the http module
-import http from "http";
-// Initialize the http server
-const server = http
-  .createServer((request, response) => {
-    // Handle the request from http://localhost:4040/status with HTTP GET method
-    if (request.url === "/status" && request.method === "GET") {
-      // Create the headers for response
-      response.writeHead(200, { "Content-Type": "application/json" });
-      // Create the response body
-      response.write(JSON.stringify({ message: "Service healthy" }));
-      // End and return the response
-      response.end();
-    }
-  })
-  // Tell the HTTP server to start listening
-  .listen(4040);
+// 'Import' the Express module instead of http
+import express from "express";
+// Initialize the Express application
+const app = express();
 
+// Handle the request with HTTP GET method from http://localhost:4040/status
+app.get("/status", (request, response) => {
+  // Create the headers for response by default 200
+  // Create the response body
+  // End and return the response
+  response.send(JSON.stringify({ message: "Service healthy" }));
+});
+app.get("/status", (request, response) => {
+  response.send(JSON.stringify({ message: "Service healthy" }));
+});
+// Tell the Express app to start listening
 // Let the humans know I am running and listening on 4040
-console.log("Listening on port 4040");
+app.listen(4040, () => console.log("Listening on port 4040"));
+
+import mongoose from "mongoose";
+mongoose.connect(process.env.MONGODB, {
+  // Configuration options to remove deprecation warnings, just include them to remove clutter
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
