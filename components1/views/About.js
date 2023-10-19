089cb366578bf93f35c3bbf6568dc8c3f93a8eed
tanyavszabo <tatianavszabo@gmail.com>
2023-10-18 23:04:39 -0400

    update

---
+++ components1/views/About.js
@@ -0,0 +1,59 @@
+import html from "html-literal";
+export default state => html`
+  <section id="About">
+    <h1>
+      About Me
+      <h1>
+        <p style="font-weight: bold">Attention catching text here...</p>
+        <div class="about-info">
+          <div class="about-img">
+            <img src="#" alt="Tanya Szabo" />
+          </div>
+          <div>
+            <p>
+              <em>
+                you are offered an opportunity that could be “Your Next Big
+                Break”, you take it. That is how I found myself on a one-way
+                flight to Mexico, with a 50lbs duffle and a pair of high heels.
+                I was offered a contract to model in Mexico for the summer and
+                was determined to make a small splash in the fashion world. I
+                returned home with a few paid-for photos and a low balance. I
+                quickly learned that chances are paid for, unless you know
+                somebody who knows somebody. In the world of social media,
+                combined with the drive to create, networking should be simple.
+                However, beauty also attracts predators who spoil the art,
+                adding to the never-ending list of modeling war stories. Every
+                story had the same theme: Lack of affordability, the need for
+                experience, and the desire to establish a connection caused
+                creatives to compromise their safety. I want to build an
+                application that will be a major platform for creatives of all
+                experience levels to network and collaborate. A lot of
+                individuals either have talents that go unnoticed or potential
+                that never gets discovered. Starting out in the fashion industry
+                is challenging because it is expensive and requires experience.
+                Along with financial dependency and prerequisites, it is easy
+                for beginners to be taken advantage of or placed in unsafe
+                situations while searching for opportunities. My application is
+                called “Cobalt”- Create. Collab. Connect. The purpose of Cobalt
+                is to create a safe network of creatives (which include
+                photographers, models, fashion stylists, hair stylists, makeup
+                artists, and videographers) to collaborate with each other,
+                based on selected locations, for free. Cobalt connects members
+                of the fashion industry by allowing users to post Time for Photo
+                Job requests that provide opportunities for artists. This allows
+                each party to mutually benefit by exchanging skills, gaining
+                experience, and adding to their portfolios. Currently, safety
+                isn't prioritized in most platforms' creative use. I plan to
+                prioritize safety by implementing security features such as
+                profile verification and account approval.
+              </em>
+            </p>
+            <button>Read More...</button>
+            <footer>
+              <p> 2023 Cobalt. All Rights Reserved.</p>
+          </div>
+        </div>
+      </h1>
+    </h1>
+  </section>
+`;
---
+++ components1/views/index.js
@@ -0,0 +1,7 @@
+export { default as Home } from "./Home";
+export { default as About } from "./About";
+export { default as Connect } from "./Connect";
+export { default as Collab } from "./Collab";
+export { default as Create } from "./Create";
+export { default as Profile } from "./Profile";
+export { default as Viewnotfound } from "./Viewnotfound";
